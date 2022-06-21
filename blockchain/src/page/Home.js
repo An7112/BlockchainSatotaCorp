897b707c91aa272ec3b28d386fbbf474357a73b6
huycan <vohuy21032001@gmail.com>
2022-06-21 22:16:07 +0700

    sidebar

---
+++ blockchain/src/page/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import SideBar from '../components/SideBar/SideBar';
+
+const Home = () => (
+    <div className='home'>
+        <SideBar />
+    </div>
+);
+
+export default Home;
