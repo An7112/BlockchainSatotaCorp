fe246cd770153e1cb109d039528119b9545d6e5d
huycan <vohuy21032001@gmail.com>
2022-06-21 23:48:50 +0700

    homepage header

--- blockchain/src/page/Home.js
+++ blockchain/src/page/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+
 import SideBar from '../components/SideBar/SideBar';
+import HomeBody from '../components/HomeBody/HomeBody';
 
 const Home = () => (
     <div className='home'>
         <SideBar />
+        <HomeBody />
     </div>
 );
 
