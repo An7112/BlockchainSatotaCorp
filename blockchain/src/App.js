157082359e23272467936d9858a27c9a5b188f1a
Đạt Lê <76443522+datgau3110@users.noreply.github.com>
2022-06-22 18:45:56 +0700

    Add files via upload

--- blockchain/src/App.js
+++ blockchain/src/App.js
@@ -1,14 +1,21 @@
 
 import './App.css';
-import Account_popup from './components/Account_popup';
-
+import Account_popup from './components/AccountPopup/Account_popup';
+import Notifications from './components/Notifications/Notifications';
+import { BrowserRouter , Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Account_popup/>
+    <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<Account_popup/>}></Route>
+      <Route path='/Notifications' element={<Notifications/>} ></Route>
 
-    </div>
+     </Routes>
+    </BrowserRouter>
+     
+
+    
   );
 }
 
