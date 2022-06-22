850c99a71842f50ddbb7dbd73d5db0ee1d09ef6a
huycan <vohuy21032001@gmail.com>
2022-06-22 17:59:03 +0700

    add component notify

--- blockchain/src/components/HomeBody/HomeBody.js
+++ blockchain/src/components/HomeBody/HomeBody.js
@@ -4,6 +4,7 @@ import APY from '../APY/APY';
 import Portfolio from '../Portfolio/Portfolio';
 import Fund from '../Fund/Fund';
 import Assets from '../Assets/Assets';
+import Notify from '../Notify/Notify';
 
 import {images} from '../../constants';
 import './Homebody.css';
@@ -53,61 +54,89 @@ const HomeBody = () => (
             <Fund />
         </div>
 
-        <div className='assets__container'>
-            <div className='assets__head'>
-                <p className='assets__head-title'>Assets</p>
-                <p className='assets__head-title_sub'>in vaults</p>
-                <p className='assets__head-desc'>outside vaults</p>
+        <div className='wrapper__assets-notify'>
+            <div className='assets__container'>
+                <div className='assets__head'>
+                    <p className='assets__head-title'>Assets</p>
+                    <p className='assets__head-title_sub'>in vaults</p>
+                    <p className='assets__head-desc'>outside vaults</p>
+                </div>
+
+                <div className='assets__content'>
+                    <div className='assets__content-list'>
+                        <p className='assets__content-item_name'>Name</p>
+                        <p className='assets__content-item_balance'>Balance</p>
+                        <p className='assets__content-item_interest'>
+                            Interest
+                        </p>
+                        <p className='assets__content-item_allocation'>
+                            Allocation
+                        </p>
+                    </div>
+                    <Assets
+                        src={images.USDCIcon}
+                        title='USD Coin'
+                        sub='USDC'
+                        balancePrice='$131,359.34'
+                        balanceSub='131,359.34 USDC'
+                        interestPrice='$9,131.78'
+                        interestSub='+$9,131.78'
+                        percent='38.3%'
+                    />
+                    <Assets
+                        src={images.BTCIcon}
+                        title='Bitcoin'
+                        sub='BTC'
+                        balancePrice='$112,193.19'
+                        balanceSub='3.81 BTC'
+                        interestPrice='$9,131.78'
+                        interestSub='+$9,131.78'
+                        percent='32.8%'
+                    />
+                    <Assets
+                        src={images.AVAXIcon}
+                        title='USD Coin'
+                        sub='USDC'
+                        balancePrice='$131,359.34'
+                        balanceSub='131,359.34 USDC'
+                        interestPrice='$9,131.78'
+                        interestSub='+$9,131.78'
+                        percent='12.1%'
+                    />
+                    <Assets
+                        src={images.DOTIcon}
+                        title='USD Coin'
+                        sub='USDC'
+                        balancePrice='$131,359.34'
+                        balanceSub='131,359.34 USDC'
+                        interestPrice='$9,131.78'
+                        interestSub='+$9,131.78'
+                        percent='7.7%'
+                    />
+                </div>
             </div>
 
-            <div className='assets__content'>
-                <div className='assets__content-list'>
-                    <p className='assets__content-item_name'>Name</p>
-                    <p className='assets__content-item_balance'>Balance</p>
-                    <p className='assets__content-item_interest'>Interest</p>
-                    <p className='assets__content-item_allocation'>
-                        Allocation
-                    </p>
-                </div>
-                <Assets
-                    src={images.USDCIcon}
-                    title='USD Coin'
-                    sub='USDC'
-                    balancePrice='$131,359.34'
-                    balanceSub='131,359.34 USDC'
-                    interestPrice='$9,131.78'
-                    interestSub='+$9,131.78'
-                    percent='38.3%'
+            <div className='notify__container'>
+            <p className='notify__title'>Notifications</p>
+                <Notify
+                    status='Account activity'
+                    action='You added 3,000 USDC'
+                    time='May 11'
                 />
-                <Assets
-                    src={images.BTCIcon}
-                    title='Bitcoin'
-                    sub='BTC'
-                    balancePrice='$112,193.19'
-                    balanceSub='3.81 BTC'
-                    interestPrice='$9,131.78'
-                    interestSub='+$9,131.78'
-                    percent='32.8%'
+                <Notify
+                    status='Account activity'
+                    action='You added 3,000 USDC'
+                    time='May 11'
                 />
-                <Assets
-                    src={images.AVAXIcon}
-                    title='USD Coin'
-                    sub='USDC'
-                    balancePrice='$131,359.34'
-                    balanceSub='131,359.34 USDC'
-                    interestPrice='$9,131.78'
-                    interestSub='+$9,131.78'
-                    percent='12.1%'
+                <Notify
+                    status='Account activity'
+                    action='You added 3,000 USDC'
+                    time='May 11'
                 />
-                <Assets
-                    src={images.DOTIcon}
-                    title='USD Coin'
-                    sub='USDC'
-                    balancePrice='$131,359.34'
-                    balanceSub='131,359.34 USDC'
-                    interestPrice='$9,131.78'
-                    interestSub='+$9,131.78'
-                    percent='7.7%'
+                <Notify
+                    status='Account activity'
+                    action='You added 3,000 USDC'
+                    time='May 11'
                 />
             </div>
         </div>
