b2775402e3c0e82c68194cb9dd6f0e19af43d145
Đạt Lê <76443522+datgau3110@users.noreply.github.com>
2022-06-24 14:26:06 +0700

    Add files via upload

--- blockchain/src/App.js
+++ blockchain/src/App.js
@@ -1,21 +1,28 @@
+import React from 'react';
+import Home from './page/Home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AccountPopup from './components/AccountPopup/AccountPopup';
+import Notifications from './components/Notifications/Notifications';
+import Referrals from './components/Referrals/Referrals';
+import Settings from './components/Settings/Settings';
+
 
 import './App.css';
-import Account_popup from './components/AccountPopup/Account_popup';
-import Notifications from './components/Notifications/Notifications';
-import { BrowserRouter , Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
+
     <BrowserRouter>
-     <Routes>
-      <Route path='/' element={<Account_popup/>}></Route>
-      <Route path='/Notifications' element={<Notifications/>} ></Route>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/AccountPopup' element={<AccountPopup />}></Route>
+        <Route path='/Settings' element={<Settings />}></Route>
+        <Route path='/Notifications' element={<Notifications />} ></Route>
+        <Route path='/Referrals' element={<Referrals />} ></Route>
 
-     </Routes>
+      </Routes>
     </BrowserRouter>
-     
-
-    
   );
 }
 
