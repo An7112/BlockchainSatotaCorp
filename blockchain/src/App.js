4478a9401e30d3f8147af98c07956fbaf5008a30
datgau3110 <letiendat3110@gmail.com>
2022-06-27 11:13:52 +0700

    edit

--- blockchain/src/App.js
+++ blockchain/src/App.js
@@ -2,9 +2,7 @@ import React from 'react';
 import Home from './page/Home';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AccountPopup from './components/AccountPopup/AccountPopup';
-import Notifications from './components/Notifications/Notifications';
-import Referrals from './components/Referrals/Referrals';
-import Settings from './components/Settingss/Settings';
+
 
 
 import './App.css';
@@ -17,9 +15,6 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/AccountPopup' element={<AccountPopup />}></Route>
-        <Route path='/Settings' element={<Settings />}></Route>
-        <Route path='/Notifications' element={<Notifications />} ></Route>
-        <Route path='/Referrals' element={<Referrals />} ></Route>
 
       </Routes>
     </BrowserRouter>
--- blockchain/src/components/AccountPopup/AccountPopup.js
+++ blockchain/src/components/AccountPopup/AccountPopup.js
@@ -1,28 +1,43 @@
 import React from 'react'
 import './AccountPopup.css'
 import { images } from '../../constants'
+import '../../page/Home'
 import { Link } from 'react-router-dom'
+import Home from '../../page/Home'
 
 export default function AccountPopup({ children, open, onClose }) {
     if (!open) return null
     return (
-        <div className='home'>
+        <div className={Home}>
             <div className="dropdown" >
-                <div className="dropdown-rectangle"></div>
-                <img src={images.Close} onClick={onClose} alt='' className="img-close" />
-                <div className="profile-pic"></div>
-                <img src={images.Avata} alt='' className='profile-pic' />
-                <div><p className="text-paren">@cameron</p></div>
-                <div><p className="text-child">cameron@email.com</p></div>
-                <div className='button-rectangle'></div>
-                <Link to='/Settingss' className='text-settings'>Settings</Link>
-                <a href='#' className='text doc'>Docs</a>
+                <div className='dropdown-box-close'>
+                    <div className="dropdown-rectangle">
+                        <img src={images.Close} onClick={onClose} alt='' className="img-close" />
+                    </div>
+                </div>
+
+                <div className="dropdown-box-avata">
+                    <div className="dropdown-profile-pic">
+                        <img src={images.Avata} alt='' className='profile-pic' />
+                    </div>
+                </div>
+                <div>
+                    <p className="text-paren">@cameron</p>
+                </div>
+                <div>
+                    <p className="text-child">cameron@email.com</p>
+                </div>
+                <div className='button-rectangle'>
+                    <a href='#' className='text-settings'>Settings</a>
+                </div>
+
+                <a href='#' className='text'>Docs</a>
                 <div className='line-acc line1'></div>
-                <Link to='/Referrals' className='text referrals'>Referrals</Link>
+                <a href='#' className='text'>Referrals</a>
                 <div className='line-acc line2'></div>
-                <a href='#' className='text help-center'>Help Center</a>
+                <a href='#' className='text'>Help Center</a>
                 <div className='line-acc line3'></div>
-                <Link to='/Notifications' className='text notifications'>Notifications</Link>
+                <a href='#' className='text'>Notifications</a>
                 <div className='line-acc line4'></div>
                 <a href='#' className='text sign-out'>Sign out</a>
             </div>
